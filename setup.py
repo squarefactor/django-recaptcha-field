ab4397ae0fced0b9863f875833d3cecbac959cbf
Bill Evans <bill@squarefactor.com>
2011-06-29 15:53:32 -0400

    setup file for pip goodness

---
+++ setup.py
@@ -0,0 +1,25 @@
+from setuptools import setup, find_packages
+
+setup(
+    name='django-recaptcha',
+    version='0.1',
+    description='Django recaptcha form field/widget app.',
+    long_description = open('README.markdown', 'r').read(),
+    author='Praekelt Foundation',
+    license='BSD',
+    url='https://github.com/jordanorelli/django-recaptcha-field.git',
+    packages = find_packages(),
+    install_requires = [
+        'recaptcha-client'
+    ],
+    include_package_data=True,
+    classifiers = [
+        "Programming Language :: Python",
+        "License :: OSI Approved :: BSD License",
+        "Development Status :: 4 - Beta",
+        "Operating System :: OS Independent",
+        "Framework :: Django",
+        "Intended Audience :: Developers",
+        "Topic :: Internet :: WWW/HTTP :: Dynamic Content",
+    ],
+)
